193f75819bf8b33e8bcadaee2e6b9afa6beae2e6
markusemile <emile.markus@gmail.com>
2025-07-09 15:17:01 +0200

    add verbose to sort_folder exerice

--- sort_folder_by_ext.py
+++ sort_folder_by_ext.py
@@ -4,20 +4,26 @@ from pathlib import Path
 # sort a files into a folder by they extensions
 
 folder_path = Path.home() / "Downloads"
+# get all elements in folder and put into list
 all_elems = list(folder_path.glob("*"))
+# initialisation of a set (no duplicate elem)
 all_extension = set()
+# get all folder and sorted
 fld = [f for f in all_elems if f.is_dir()]
 fld.sort()
+# get all files and sorted
 files = [f for f in all_elems if f.is_file()]
 files.sort()
 
-for item in all_elems:
-    if item.is_file():
-        all_extension.add(item.suffix)
+# get all suffix from files
+for item in files:
+    all_extension.add(item.suffix)
 
-all_extension = [item for item in all_extension]
+# convert to list and sort the extension
+all_extension = list([item for item in all_extension])
 all_extension.sort()
 
+# dictionaries of extension with its full name
 extension_name ={
     ".png": "Image",
     ".jpg": "Image",
@@ -37,10 +43,10 @@ extension_name ={
     ".msi": "Executable",
     ".jar": "Java"
 }
-
+# display folders at first
 for folder in fld:
     print(f"[Folder]: {folder.name.capitalize()}")
-
+# next we display the files sorted by extension name and then by alpha
 for ext in all_extension:
     for file in files:
         if file.suffix == ext:
